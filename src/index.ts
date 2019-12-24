9f8f736a7876ce0f07be03988f054189f93767d5
Jonathon Alder <jonathon.alder@gmail.com>
2019-12-24 15:34:53 -0500

    ffs -> index, fs functions

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,72 @@
-import { exit } from './error';
+const fs = require('fs');
+const { spawnSync } = require('child_process');
+const { exit } = require('./error');
 
 /*
 CLI flow
 ffs <project-name> [args] [/path/to/directory] 
   args:
     --framework: react, vue, angular
-    --bundler: *webpack, browserify, rollup
-    -c, --configure: true, *false
-    -v, --verbose: true, *false
-with args:
-  verify args and install
-  configure if specified
-without args:
-  prompt for framework
-  prompt for bundler
-  install packages
-  prompt for configuration
-  configure and install packages
+    --bundler: (webpack), browserify, rollup
+    -c, --configure: boolean (false)
+    -g, --git: boolean (false)
+    -v, --verbose: boolean (false)
+1. create directory
+2. prompt if necessary and install packages
+3. 
 */
 
 interface BaseSettings {
+  root: string;
   name: string | null;
   framework: string | null;
   bundler: string | null;
   configure: boolean;
+  git: boolean;
   verbose: boolean;
 }
 
-const settings = {
+/* --- DEFAULT SETTINGS --- */
+
+const settings: BaseSettings = {
+  root: process.env.PWD,
   name: null,
   framework: null,
   bundler: null,
   configure: false,
+  git: false,
   verbose: false,
 };
 
-/* --- CONFIGURE SETTINGS --- */
+let path = null;
+
+/* --- FILE SYSTEM --- */
+
+const mkdir = (): void => {
+  if (settings.verbose) {
+    console.log(`creating directory: ${path}...`);
+  }
+  fs.mkdirSync(path);
+};
+
+const writeFile = (pathWithName: string, contents: string): void => {
+  fs.writeFileSync(pathWithName, contents);
+}
+
+const scaffold = (): void => {
+  mkdir();
+  process.chdir(path);
+  fs.writeFileSync(`${path}/package.json`, "FOO");
+};
+
+
+/* --- NPM --- */
+
+const install = (args: Array<string>): void => spawnSync('npm', ['install', ...args, '--save']);
+
+const installDev = (args: Array<string>): void => spawnSync('npm', ['install', ...args, '--save-dev']);
+
+/* --- SETTINGS --- */
 
 function setName(str?: string): void {
   if (typeof str === 'undefined') {
@@ -45,6 +76,7 @@ function setName(str?: string): void {
     exit('Error: Project name does not meet naming conventions. See readme for help.');
   }
   settings.name = str;
+  path = `${settings.root}/${str}`;
 }
 
 function setOpt(opt: string): void {
@@ -60,6 +92,8 @@ function setOpt(opt: string): void {
 function setFlag(flag: string): void {
   if (flag === 'c') {
     settings.configure = true;
+  } else if (flag === 'g') {
+    settings.git = true;
   } else if (flag === 'v') {
     settings.verbose = true;
   } else {
@@ -76,6 +110,7 @@ function parseArgs(args?: Array<string>): void {
         for (let j = 1, len = args[i].length; j < len; j++) {
           setFlag(args[i].charAt(j));
         }
+      // TODO parse installation path
       } else {
         exit(`Error: ${args[i]} is not a valid argument.`);
       }
@@ -88,4 +123,5 @@ function parseArgs(args?: Array<string>): void {
 const args = process.argv.slice(2);
 setName(args[0]);
 parseArgs(args.slice(1));
+scaffold();
 
