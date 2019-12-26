bc1c1cb8f6fbf8e7373d46301260a7fdd819d953
Jonathon Alder <jonathon.alder@gmail.com>
2019-12-26 15:59:31 -0500

    package dependencies definition

---
+++ src/dependencies.ts
@@ -0,0 +1,30 @@
+type Dependencies = {
+  [key: string]: {
+    main: Array<string>;
+    dev: Array<string>;
+  };
+};
+
+export const dependencies: Dependencies = {
+  react: {
+    main: ['react', 'react-dom'],
+    dev: ['babel', 'babel-loader', 'babel-preset-react'],
+  },
+  webpack: {
+    main: [],
+    dev: [],
+  },
+  rollup: {
+    main: [],
+    dev: [],
+  },
+  browserify: {
+    main: [],
+    dev: [],
+  },
+  parcel: {
+    main: [],
+    dev: [],
+  },
+}
+
