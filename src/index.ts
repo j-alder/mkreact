6d76dc6f2282b4c7b7eadb8d511f0a8a47624bab
Jonathon Alder <jonathon.alder@gmail.com>
2019-12-26 15:59:48 -0500

    config mutation updates

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,36 @@
 const fs = require('fs');
 const { spawnSync } = require('child_process');
 const { exit } = require('./error');
+const { dependencies } = require('./dependencies');
+const { yesNo, multiChoice } = require('./prompts');
 
-/*
-CLI flow
-ffs <project-name> [args] [/path/to/directory] 
-  args:
-    --framework: react, vue, angular
-    --bundler: (webpack), browserify, rollup
-    -c, --configure: boolean (false)
-    -g, --git: boolean (false)
-    -v, --verbose: boolean (false)
-1. create directory
-2. prompt if necessary and install packages
-3. 
-*/
+/* --- DEFAULT CONFIG --- */
 
-interface BaseSettings {
-  root: string;
+type BaseConfig = {
+  currentDir: string;
   name: string | null;
+  path: string | null;
   framework: string | null;
   bundler: string | null;
-  configure: boolean;
-  git: boolean;
+  configure: boolean | null;
+  git: boolean | null;
   verbose: boolean;
-}
+};
 
-/* --- DEFAULT SETTINGS --- */
-
-const settings: BaseSettings = {
-  root: process.env.PWD,
+// initialize global starting config
+// TODO find a better way to store global config?
+const config: BaseConfig = {
+  currentDir: process.env.PWD,
   name: null,
+  path: null,
   framework: null,
   bundler: null,
-  configure: false,
-  git: false,
+  configure: null,
+  git: null,
   verbose: false,
 };
 
-let path = null;
-
-/* --- FILE SYSTEM --- */
-
-const mkdir = (): void => {
-  if (settings.verbose) {
-    console.log(`creating directory: ${path}...`);
-  }
-  fs.mkdirSync(path);
-};
-
-const writeFile = (pathWithName: string, contents: string): void => {
-  fs.writeFileSync(pathWithName, contents);
-}
-
-const scaffold = (): void => {
-  mkdir();
-  process.chdir(path);
-  fs.writeFileSync(`${path}/package.json`, "FOO");
-};
-
-
-/* --- NPM --- */
-
-const install = (args: Array<string>): void => spawnSync('npm', ['install', ...args, '--save']);
-
-const installDev = (args: Array<string>): void => spawnSync('npm', ['install', ...args, '--save-dev']);
-
-/* --- SETTINGS --- */
+/* --- CONFIG MUTATION --- */
 
 function setName(str?: string): void {
   if (typeof str === 'undefined') {
@@ -75,15 +39,15 @@ function setName(str?: string): void {
   if (!/^[\w]+(\w|-|_)*[\w]+$/i.test(str)) {
     exit('Error: Project name does not meet naming conventions. See readme for help.');
   }
-  settings.name = str;
-  path = `${settings.root}/${str}`;
+  config.name = str;
+  config.path = `${config.currentDir}/${str}`;
 }
 
-function setOpt(opt: string): void {
+function setArg(opt: string): void {
   if (/^--framework=(react|vue|angular)$/.test(opt)) {
-    settings.framework = opt.split('=')[1];
+    config.framework = opt.split('=')[1];
   } else if (/^--bundler=(webpack|browserify|rollup)$/.test(opt)) {
-    settings.bundler = opt.split('=')[1];
+    config.bundler = opt.split('=')[1];
   } else {
     exit(`Error: "${opt}" is not a valid argument.`);
   }
@@ -91,11 +55,11 @@ function setOpt(opt: string): void {
 
 function setFlag(flag: string): void {
   if (flag === 'c') {
-    settings.configure = true;
+    config.configure = true;
   } else if (flag === 'g') {
-    settings.git = true;
+    config.git = true;
   } else if (flag === 'v') {
-    settings.verbose = true;
+    config.verbose = true;
   } else {
     exit(`Error: "${flag}" is not a valid argument.`);
   }
@@ -105,12 +69,11 @@ function parseArgs(args?: Array<string>): void {
   if (typeof args !== 'undefined') {
     for (let i = 0, len = args.length; i < len; i++) {
       if (/^(--).*$/.test(args[i])) {
-        setOpt(args[i]);
+        setArg(args[i]);
       } else if (/^(-).*$/.test(args[i])) {
         for (let j = 1, len = args[i].length; j < len; j++) {
           setFlag(args[i].charAt(j));
         }
-      // TODO parse installation path
       } else {
         exit(`Error: ${args[i]} is not a valid argument.`);
       }
@@ -118,10 +81,79 @@ function parseArgs(args?: Array<string>): void {
   }
 }
 
+/* --- NPM --- */
+
+const install = (args: Array<string>): void => {
+  if (config.verbose) {
+    console.log(`installing: ${args.join(', ')}...`);
+  }
+  spawnSync('npm', ['install', ...args, '--save']);
+};
+
+const installDev = (args: Array<string>): void => {
+  if (config.verbose) {
+    console.log(`installing ${args.join(', ')}...`);
+  }
+  spawnSync('npm', ['install', ...args, '--save-dev']);
+}
+
+
+/* --- FILE SYSTEM --- */
+
+/** Create project directory and package.json */
+const scaffold = (): void => {
+  if (config.verbose) console.log(`creating directory: ${config.path}...`);
+  fs.mkdirSync(config.path);
+  process.chdir(config.path);
+  fs.writeFileSync(`${config.path}/package.json`, JSON.stringify({
+    name: config.name,
+    author: '',
+    version: '0.1.0',
+    main: 'index.js',
+    license: 'UNLICENSED',
+  }, null, 2));
+};
+
+/* --- PACKAGE CONFIG --- */
+
+function setOptions() {
+  if (config.framework === null) {
+    config.framework = multiChoice(
+      'Which application framework are you using?',
+      ['react', 'vue'],
+      'react'
+    );
+  }
+  if (config.bundler === null) {
+    config.bundler = multiChoice(
+      'Which bundler would you like to install?',
+      ['none', 'webpack', 'browserify', 'rollup', 'parcel'],
+      'webpack'
+    );
+  }
+  if (config.git === null) {
+    config.git = yesNo('Initialize git repository?', false);
+  }
+  console.log(config);
+}
+
+
 /* --- MAIN --- */
 
-const args = process.argv.slice(2);
-setName(args[0]);
-parseArgs(args.slice(1));
-scaffold();
+setOptions();
+
+//const args = process.argv.slice(2);
+//setName(args[0]);                             // set project name
+//parseArgs(args.slice(1));                     // read and set arguments and flags
+//scaffold();                                   // create dir + package.json
+//promptForInstallationCandidates();
+//install([                                     // install dependencies
+//  ...dependencies[config.framework].main,
+//  ...dependencies[config.bundler].main,
+//]);
+//installDev([                                  // install dev dependencies
+//  ...dependencies[config.framework].main,
+//  ...dependencies[config.bundler].main,
+//]);
+//configure();
 
