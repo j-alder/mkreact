9d54a27a29be22e6fed0971ae98569dce9fea710
Jonathon Alder <jonathon.alder@gmail.com>
2019-12-28 20:57:16 -0500

    promise fix

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,47 +1,58 @@
 const { exit } = require('./error');
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 function ask(question: string): Promise<string> {
-  let response: string;
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
   rl.setPrompt(question);
   rl.prompt();
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
+    let response: string;
     rl.on('line', (input: string) => {
       response = input;
       rl.close();
     });
     rl.on('close', () => resolve(response));
-    rl.on('error', () => reject());
   });
 }
 
 export async function yesNo(question: string, def: boolean = false): Promise<boolean> {
   question = `${question} [${def === true ? 'Y' : 'y'}|${def === false ? 'N' : 'n'}] `;
-  await ask(question)
-    .then((input: string) => input === 'y' || input === 'Y' || def);
+  return ask(question)
+    .then((res: string) => {
+      if (res === 'y' || res === 'Y') {
+        return true;
+      } else if (res === 'n' || res === 'N') {
+        return false;
+      } else {
+        return def;
+      }
+    });
 }
 
 function fmtOptions(options: Array<string>): string {
   let result = '\n';
   for (let i = 0, len = options.length; i < len; i++) {
-    result += `  ${i}. ${options[i]}` + '\n';
+    result += `  ${i}. ${options[i] || 'none'}` + '\n';
   }
   return result += `[0-${options.length-1}]: `;
 }
 
-export async function multiChoice(question: string, options: Array<string>, def: string|null = null): Promise<string|null> {
+export async function multiChoice(
+  question: string,
+  options: Array<string>,
+  def: string|null = null
+): Promise<string|null> {
   question += (def ? ` (default: ${def})` : '') + fmtOptions(options);
-  await ask(question)
+  return ask(question)
     .then((res: string) => {
       if (res !== '') {
         const index = parseInt(res);
         return options[index] || def;
       }
       return def;
-    })
+    });
 }
+
