0ec80131a982637f44a47ab912c89ed0f80d54a6
Jonathon Alder <jonathon.alder@gmail.com>
2020-02-10 07:00:01 -0500

    updated webpack settings

--- files/webpack/webpack.config.js
+++ files/webpack/webpack.config.js
@@ -1,7 +1,26 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+  },
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
